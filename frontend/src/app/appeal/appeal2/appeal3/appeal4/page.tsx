a8936710b59067062a93041c6dab7d1868ecb75f
olegkozachenko <olegkozd@yandex.ru>
2026-09-09 15:17:07 +0500

    фикс хеддер

--- frontend/src/app/appeal/appeal2/appeal3/appeal4/page.tsx
+++ frontend/src/app/appeal/appeal2/appeal3/appeal4/page.tsx
@@ -8,10 +8,8 @@ export default async function AppealDetails({ searchParams }: {
   const role = getAppealRole((await searchParams).role);
 
   return (
-    <main className="flex min-h-dvh min-w-[900px] flex-col bg-[#f7f9fe] text-[#11131a]">
-      <div className="[&_header]:border-x [&_header]:border-t [&_header]:border-[#9dacf9] [&_header>div]:min-h-[88px] [&_header>div]:max-w-none [&_header>div]:px-[18px] [&_header>div]:py-4 [&_header_img]:w-[160px] [&_header_p]:text-[15px] [&_header_p]:leading-[1.45] [&_header_p]:font-medium">
-        <SiteHeader />
-      </div>
+    <main className="flex min-h-dvh flex-col bg-[#f7f9fe] text-[#11131a]">
+      <SiteHeader />
 
       <ClarifyingQuestions role={role.id} />
     </main>
